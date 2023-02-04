18000de6cb3555b1bfa2d3d0a3950aec22f50e85
Mrd7 <54204775+mrd17@users.noreply.github.com>
2023-02-04 20:51:25 +0530

    Create run.sh
    This script downloads httpx,subfinder,naabu,ffuf, seclists, go.

---
+++ run.sh
@@ -0,0 +1,18 @@
+ #!/bin/bash
+
+snap install go --classic
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+git clone https://github.com/danielmiessler/SecLists.git
+go install github.com/ffuf/ffuf@latest
+
+cd go/bin
+
+mv httpx /usr/local/bin/ 
+mv fluf /usr/local/bin/ 
+mv subfinder /usr/local/bin/ 
+mv naabu  /usr/local/bin/
+
+
+chmod +x run.sh 
